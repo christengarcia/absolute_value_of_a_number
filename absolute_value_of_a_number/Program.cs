24b47b66279876d5021e45ba26475a20627aa442
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-27 21:39:20 +1100

    Initial commit

---
+++ absolute_value_of_a_number/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  C# Program to Display Absolute value of a Number
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace absolute_value_of_a_number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter a number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num < 0)
+            {
+                num = num * -1;
+            }
+
+            Console.WriteLine("Absolute value : " + num);
+            Console.ReadLine();
+        }
+    }
+}
